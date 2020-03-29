112562502f7dea6704b3c0971c2ccd09d55806bd
willian casanova <casanova.uill@gmail.com>
2020-03-28 23:49:46 -0300

    Fogo do Doom

---
+++ god/fire.js
@@ -0,0 +1,31 @@
+const firePixelsArray = []
+const fireWidth = 2
+const fireHeight = 3
+
+
+
+function start() {
+    createFireDataStructure()
+    console.log(firePixelsArray)
+
+}
+
+
+function createFireDataStructure() {
+const numberOfPixels = fireWidth * fireHeight
+
+    for (let i = 0; i < numberOfPixels; i++) {
+        firePixelsArray[i] = 0
+    }
+}
+
+function calculateFirePropagation() {  
+
+}
+
+function renderFire() {
+
+}
+
+
+start ()
